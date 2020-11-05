d3a4050ef94f55cec91ebaf607a1f481fc496049
abernier <antoine.bernier@gmail.com>
2020-11-05 16:18:31 +0100

    iteration 4

--- src/App.js
+++ src/App.js
@@ -13,7 +13,8 @@ import AddFood from './components/AddFood'
 class App extends React.Component {
   state = {
     foods: json,
-    addingFood: false
+    addingFood: false,
+    query: ''
   }
 
   addFood = (newFood) => {
@@ -24,7 +25,22 @@ class App extends React.Component {
     });
   }
 
+  // Update the `query` state on change
+  handleQuery = (ev) => {
+    this.setState({
+      query: ev.target.value
+    })
+  }
+
   render() {
+    let foods = this.state.foods;
+    const query = this.state.query;
+
+    // Filter `foods` with `query`
+    if (query) {
+      foods = foods.filter(food => food.name.includes(query))
+    }
+
     return (
       <div className="App">
         {
@@ -33,8 +49,11 @@ class App extends React.Component {
           :
           <button onClick={ev => this.setState({addingFood: true})}>Add Food</button>
         }
+
+        <input type="search" value={this.state.query} onChange={this.handleQuery} />
+
   
-        {this.state.foods.map(food => (
+        {foods.map(food => (
           <Foodbox key={food.name} name={food.name} image={food.image} calories={food.calories} />
         ))}
         
