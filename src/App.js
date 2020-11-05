6a3065bda29504ef95612b8129c6e70eb504d838
abernier <antoine.bernier@gmail.com>
2020-11-05 15:42:19 +0100

    iteration 0

--- src/App.js
+++ src/App.js
@@ -2,6 +2,10 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import 'bulma/css/bulma.css';
+
+import json from './foods.json';
+
 function App() {
   return (
     <div className="App">
