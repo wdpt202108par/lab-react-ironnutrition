8aea9dbe759e7e94a6fa96984254dd235798358b
abernier <antoine.bernier@gmail.com>
2020-11-05 15:50:47 +0100

    Iteration 1

--- src/App.js
+++ src/App.js
@@ -6,23 +6,12 @@ import 'bulma/css/bulma.css';
 
 import json from './foods.json';
 
+import Foodbox from './components/Foodbox'
+
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Foodbox name="Pizza" image="https://i.imgur.com/eTmWoAN.png" calories={400} />
     </div>
   );
 }
