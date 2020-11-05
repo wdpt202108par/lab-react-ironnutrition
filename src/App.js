0f18c64a9bdfd1b703dfc41c4388dd8fa441cefb
abernier <antoine.bernier@gmail.com>
2020-11-05 16:15:35 +0100

    iteration 3

--- src/App.js
+++ src/App.js
@@ -7,16 +7,40 @@ import 'bulma/css/bulma.css';
 import json from './foods.json';
 
 import Foodbox from './components/Foodbox'
+import AddFood from './components/AddFood'
 
-function App() {
-  return (
-    <div className="App">
-      {json.map(food => (
-        <Foodbox key={food.name} name={food.name} image={food.image} calories={food.calories} />
-      ))}
-      
-    </div>
-  );
+
+class App extends React.Component {
+  state = {
+    foods: json,
+    addingFood: false
+  }
+
+  addFood = (newFood) => {
+    // Add `newFood` to `foods` state
+    this.setState({
+      foods: [...this.state.foods, newFood],
+      addingFood: false
+    });
+  }
+
+  render() {
+    return (
+      <div className="App">
+        {
+          this.state.addingFood ?
+          <AddFood onAdd={this.addFood} />
+          :
+          <button onClick={ev => this.setState({addingFood: true})}>Add Food</button>
+        }
+  
+        {this.state.foods.map(food => (
+          <Foodbox key={food.name} name={food.name} image={food.image} calories={food.calories} />
+        ))}
+        
+      </div>
+    );
+  }
 }
 
 export default App;
