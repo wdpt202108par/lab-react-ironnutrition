94c40f4575b477ac3daa84d26e24073fcc9e373d
Lea Beaulieu <beaulieu.lea@free.fr>
2022-01-15 15:09:06 +0100

    done iteration 0

--- src/App.js
+++ src/App.js
@@ -2,6 +2,11 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import 'bulma/css/bulma.css';
+
+import foods from './foods.json';
+
+
 function App() {
   return (
     <div className="App">
