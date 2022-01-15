180347dc2e55e1d62515e1ab91d680cdc34e085e
Lea Beaulieu <beaulieu.lea@free.fr>
2022-01-15 16:03:37 +0100

    done iteration 1

--- src/App.js
+++ src/App.js
@@ -5,25 +5,21 @@ import './App.css';
 import 'bulma/css/bulma.css';
 
 import foods from './foods.json';
+import Foodbox from './components/Foodbox';
+
 
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <h1>Let's see our foodbox</h1>
+    
+    {foods.map( (el) => {
+      return (<Foodbox image={el.image} name={el.name} calories={el.calories} quantity={el.quantity}/>)
+    })
+
+    }
+    
     </div>
   );
 }
