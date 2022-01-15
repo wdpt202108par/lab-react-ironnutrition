b2eb005ebc6197ad2ff5eb332b6c99793827929b
Yooyoo56 <tak.yoojeong@gmail.com>
2022-01-15 15:42:09 +0100

    iteration 2 done

--- src/App.js
+++ src/App.js
@@ -1,26 +1,59 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+//import logo from './logo.svg';
 import './App.css';
+import 'bulma/css/bulma.css';
+import foods from './foods.json';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class Foodbox extends Component {
+  render() {
+    return (
+      <div className="box">
+        <article className="media">
+          <div className="media-left"></div>
+          <figure className="image is-64x64">
+            <img src={this.props.image} alt="food-img"></img>
+            <p>
+              <strong>{this.props.name}</strong>
+              <br />
+              <small>{this.props.calories}cal</small>
+            </p>
+          </figure>
+          <div className="media-content">
+            <div className="content"></div>
+          </div>
+          <div className="media-right">
+            <div className="field has-addons">
+              <div className="control">
+                <input className="input" type="number" value="1" />
+              </div>
+              <div className="control">
+                <button className="button is-info">+</button>
+              </div>
+            </div>
+          </div>
+        </article>
+      </div>
+    );
+  }
+}
+
+class App extends Component {
+  state = {
+    foods: foods,
+  };
+  render() {
+    return (
+      <div className="App">
+        {this.state.foods.map((food) => (
+          <Foodbox
+            image={food.image}
+            name={food.name}
+            calories={food.calories}
+          />
+        ))}
+      </div>
+    );
+  }
 }
 
 export default App;
