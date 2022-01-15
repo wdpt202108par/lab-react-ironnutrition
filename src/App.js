57cac1449193b0acba5c483f54bf9c661b395516
aude.claquesin <aude.claquesin@gmail.com>
2022-01-15 17:07:37 +0100

    Iterations 1 to 4

--- src/App.js
+++ src/App.js
@@ -1,26 +1,114 @@
 import React from 'react';
-import logo from './logo.svg';
+import 'bulma/css/bulma.css';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import FoodBox from './foodBox/FoodBox'
+import json from './foods.json'
+import { toHaveFocus } from '@testing-library/jest-dom';
+
+
+class App extends React.Component {
+  state = {
+    foods : json
+  }
+
+  addFood = (food) => {
+    const foodCopy = this.state.foods.slice(); // copy
+    foodCopy.push(food);
+
+    this.setState({
+      foods: foodCopy
+    });
+  };
+  
+  handleSubmit = (event) => {
+    event.preventDefault(); 
+    this.addFood(this.state);
+
+    // Reset formulaire
+    this.setState({
+      name: "",
+      calories: "", 
+      image: ""
+    });
+  };
+
+  handleNameInput = (event) => {
+    this.setState({ name: event.target.value });
+  };
+
+  handleCaloriesInput = (event) => {
+    this.setState({ calories: event.target.value });
+  };
+
+  handleImgInput = (event) => {
+    this.setState({ image: event.target.value });
+  };
+
+  handleSearchBox = (event) => {
+    //Récupère l'event target value capturée dans l'input
+    //Find foodboxes filtrées sur event target value
+    const foodCopy = [...json];
+
+    let foodResults = foodCopy.filter( (el) => {
+      return el.name.toLowerCase().includes(event.target.value.toLowerCase())
+      
+    })
+    this.setState({ foods: foodResults})
+  };
+
+
+  render() {
+    return (
+      <div className="App">
+        <input 
+            type="text"
+            id="searchBar"
+            name="search"
+            value={this.state.search}
+            onChange={this.handleSearchBox}
+          />
+        <form onSubmit={this.handleSubmit}>
+          <label htmlFor="foodName">Name</label>
+          <input 
+            type="text"
+            id="foodName"
+            name="Name "
+            value={this.state.name}
+            onChange={this.handleNameInput}
+          />
+          <label htmlFor="foodCalories">Calories</label>
+          <input 
+            type="number"
+            id="foodCalories"
+            name="number of calories"
+            value={this.state.calories}
+            onChange={this.handleCaloriesInput}
+          />
+          <label htmlFor="foodImg">Image</label>
+          <input 
+            type="url"
+            id="foodImg"
+            name="Image"
+            value={this.state.image}
+            onChange={this.handleImgInput}
+          />
+          <button>Submit</button>
+        </form>
+        {this.state.foods.map((el) => {
+          return(
+            
+            <FoodBox key={`${el.name}-${el.image}`}
+              image={el.image}
+              name={el.name}
+              calories={el.calories}
+            />
+          )
+        })}
+        
+      </div>
+    );
+  }  
 }
 
 export default App;
